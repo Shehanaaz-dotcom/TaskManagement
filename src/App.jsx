5e75c5ca7199b99197bf036719677c964a2e48e4
Shehanaaz-dotcom <shehanaazbanu1999@gmail.com>
2025-08-27 16:04:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes , Route , Link} from 'react-router-dom'
 import TaskList from './components/TaskList'
 import AddTask from './components/AddTask'
+import { BrowserRouter,Route,Link ,Routes} from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-       <BrowserRouter basename={"/TaskManagement/"}>
-    <Routes>
+   <BrowserRouter basename={"/TaskManagement/"}>
     <div className="min-h-screen bg-gray-100 p-4">
       <div className="max-w-2xl mx-auto bg-white shadow-md rounded-md p-6">
         <h1 className ="text-2xl font-bold mb-4 text-center text-indigo-700"> Task Management</h1>
-      <AddTask/>
-      <TaskList/>
+                <Routes>
+<Route  path="/" element={<div><AddTask/><TaskList/></div>}/>
+     
+       </Routes>
+   
       </div>
     </div>
 
-       </Routes>
- </BrowserRouter>
+    </BrowserRouter>   
   )
 }
 
